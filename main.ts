950d7b79c155722787d9ce129678382c25716b6f
MiskovskaM <115079420+MiskovskaM@users.noreply.github.com>
2023-02-16 11:33:56 +0100

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,35 @@
 radio.setGroup(3);
-radio.setFrequencyBand(15);
+//radio.setFrequencyBand(15);
+
+
+
+input.onButtonPressed(Button.A, function () {
+    RunComp.SetLightLevel()
+})
+
 
 let time = 0;
 
 radio.onReceivedValue(function (name: string, value: number) {
-    if(name == "Zapnistopky" && value == 1 )
+
+    if(name == "Zpn" && value == 1 )
     {
-        while(input.lightLevel() > 100)
-        {
-            time += 1;
-            basic.pause(1000);
-            whaleysans.showNumber(time)
-        }
-        whaleysans.showNumber(time);
-        radio.sendValue("cas", time)
+            basic.forever(function() {
+                time += 1;
+                basic.pause(1000)
+                whaleysans.showNumber(time);
+            })  
     }
 }) 
+
+if (RunComp.OnLightDrop(function ()
+{  
+    whaleysans.showNumber(time);
+    radio.sendValue("cas", time)
+
+     
+}))
+
 
 input.onButtonPressed(Button.B, function() {
     control.reset()
