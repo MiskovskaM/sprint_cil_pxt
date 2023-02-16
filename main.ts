ea51dcf731799ac6eeb2e04117bfac68523327d0
MiskovskaM <115079420+MiskovskaM@users.noreply.github.com>
2023-02-16 16:46:17 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,36 +1,39 @@
 radio.setGroup(3);
-//radio.setFrequencyBand(15);
+//radio.setFrequencyBand()
+
+let time = 0;
+let stopky = false
 
 
 
-input.onButtonPressed(Button.A, function () {
-    RunComp.SetLightLevel()
+radio.onReceivedNumber(function (receivedNumber: number) {
+    if (receivedNumber == 1) {
+        RunComp.SetLightLevel()
+    }
+    if (receivedNumber == 2) {
+        stopky = true
+        control.inBackground(function () {
+            while (stopky) {
+                time += 1;
+                basic.pause(1000);
+                whaleysans.showNumber(time)
+
+            }
+        })
+
+    }
 })
 
 
-let time = 0;
 
-radio.onReceivedValue(function (name: string, value: number) {
-
-    if(name == "Zpn" && value == 1 )
-    {
-            basic.forever(function() {
-                time += 1;
-                basic.pause(1000)
-                whaleysans.showNumber(time);
-            })  
-    }
-}) 
-
-if (RunComp.OnLightDrop(function ()
-{  
+RunComp.OnLightDrop(function () {
+    stopky = false
     whaleysans.showNumber(time);
     radio.sendValue("cas", time)
-
-     
-}))
+})
 
 
-input.onButtonPressed(Button.B, function() {
+
+input.onButtonPressed(Button.B, function () {
     control.reset()
 })
